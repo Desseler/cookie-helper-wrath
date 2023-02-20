2074d7c6744ebe7f9fa7a5dabcbf83e5032047de
Trevor Luster <desseler4@hotmail.com>
2023-02-20 13:12:15 -0600

    Update cookie-helper-wrath.js

--- cookie-helper-wrath.js
+++ cookie-helper-wrath.js
@@ -15,21 +15,38 @@
 
     setInterval(function() { Game.ClickCookie(); }, 4);
 
-// allows me to turn on and off the clicking of the Wrath Cookies
-var wrathCookieSwitchOn = true;
-
-var SpecialCookieClicker =
-    setInterval(function() {
-        if (wrathCookieSwitchOn){
-            Game.shimmers.forEach(function(shimmer) {
-                if (shimmer.type == "golden") { shimmer.pop() }
-            })
-        } else {
-            Game.shimmers.forEach(function(shimmer) {
-                if(shimmer.type == "golden" && shimmer.wrath == 0)
-                {
-                    shimmer.pop()
-                }
-            })
+setInterval(function() {
+    Game.shimmers.forEach(function(shimmer)
+    {
+        if(shimmer.type == "golden")
+        {
+            shimmer.pop()
         }
-    }, 500);
+    })
+}, 500);
+
+var autoReindeer = setInterval(function() { for (var h in Game.shimmers){if(Game.shimmers[h].type=="reindeer"){Game.shimmers[h].pop();}} }, 100);
+
+})();
+
+var autoPopTwelveth = setInterval(function() {
+    var wrinkCount = 0,
+        wrinkEaten = 0,
+        wrinkIndex = 10; // value for 10 shinies test
+
+    for (var i in Game.wrinklers) {
+        // count number of eating wrinks
+        if (Game.wrinklers[i].sucked > 0) {
+            wrinkCount += 1;
+        }
+        // find top wrink index, ignoring shiny wrinklers
+        if (Game.wrinklers[i].sucked > wrinkEaten && Game.wrinklers[i].type == 0) {
+            wrinkEaten = Game.wrinklers[i].sucked;
+            wrinkIndex = i;
+        }
+    }
+    // pop top wrinkler if 10 eating, unless all 12 are shiny
+    if (wrinkCount == 10 && wrinkIndex != 10) {
+        Game.wrinklers[wrinkIndex].hp = 0;
+    }
+}, 60000);
